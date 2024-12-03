3d718d6bb25c30f38f58b5e0f717ffb95770b0f8
Eli <elijames2301@gmail.com>
2024-12-03 00:04:59 -0500

    Write day2 part 1 solution

--- day2.py
+++ day2.py
@@ -1,9 +1,29 @@
-def is_increasing_or_decreasing():
-    pass
+def is_ascending_or_decreasing(row):
+    if all(row[i] < row[i + 1] for i in range(len(row) - 1)):
+        return True  # Strictly ascending
+    if all(row[i] > row[i + 1] for i in range(len(row) - 1)):
+        return True  # Strictly descending
+    return False
 
-def legal_adjacent_diff():
-    pass
+def legal_adjacent_diff(row):
+    #any two adjacent numbers differ by at least one and at most three
+    for i in range(len(row) - 1):
+        if abs(row[i] - row[i + 1]) > 3 or abs(row[i] - row[i + 1]) < 1:
+            return False
+    return True
+
+def solution(data):
+    safe_reports = 0
+    for row in data:
+        if is_ascending_or_decreasing(row) and legal_adjacent_diff(row):
+            safe_reports += 1
+    return safe_reports
 
 if __name__ == "__main__":
+    data = []
     with open("day2input.txt", 'r') as file:
-        lines = file.readlines()
+        for line in file:
+            row = [int(num) for num in line.split()]
+            data.append(row)
+            #print(row)
+    print(solution(data))
