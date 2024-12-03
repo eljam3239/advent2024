26ccf3ba0beb821ebcae13fd61b62c0f0e153232
Eli <elijames2301@gmail.com>
2024-12-02 23:52:21 -0500

    Add day 2 file

---
+++ day2.py
@@ -0,0 +1,9 @@
+def is_increasing_or_decreasing():
+    pass
+
+def legal_adjacent_diff():
+    pass
+
+if __name__ == "__main__":
+    with open("day2input.txt", 'r') as file:
+        lines = file.readlines()
