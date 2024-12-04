4e1c27baf4b531a7d0978a98939cf629b2a2da0c
Eli <elijames2301@gmail.com>
2024-12-03 19:56:28 -0500

    Write day3 part 2

--- day3.py
+++ day3.py
@@ -14,4 +14,22 @@ if __name__ == '__main__':
         product = int(instruction[0]) * int(instruction[1])
         sum+=product
 
+   #print(sum)
+
+    pattern2 = r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"
+    valid_instructions2 = []
+    valid_instructions2 = re.findall(pattern2, instructions)
+    enabled = True
+    sum = 0
+    for instruction in valid_instructions2:
+        match instruction[0]:
+            case "do()":
+                enabled = True
+            case "don't()":
+                enabled = False
+            case _:
+                if enabled:
+                    product = int(instruction[1]) * int(instruction[2])
+                    sum+=product
     print(sum)
+    
